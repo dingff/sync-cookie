e50029ac84bba8af9b024df3b23661e246f18848
丁非凡 <dingff@dingfeifandeMacBook-Air.local>
2025-03-17 14:20:50 +0800

    fix: no need to update _global twice

--- src/scripts/inject.js
+++ src/scripts/inject.js
@@ -1,5 +1,5 @@
 window.addEventListener('message', (event) => {
-  if (event.source === window && event.data.type === 'SET_VALUE') {
+  if (event.source === window && event.data.type === 'SET_VALUE' && !window._global) {
     console.log('_global', event.data.value)
     window._global = event.data.value
   }
